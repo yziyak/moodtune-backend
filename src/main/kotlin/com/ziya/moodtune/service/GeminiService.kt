d1f1f5da034f62d22d2665cf32f7b7e0894529ca
yziyak <yziyak9@gmail.com>
2025-11-27 15:02:59 +0300

    01

--- src/main/kotlin/com/ziya/moodtune/service/GeminiService.kt
+++ src/main/kotlin/com/ziya/moodtune/service/GeminiService.kt
@@ -21,7 +21,7 @@ class GeminiService(
         }
 
         val url =
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$geminiApiKey"
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=$geminiApiKey"
 
         val body = mapOf(
             "contents" to listOf(
